1724e2af9e3c2793b1e68f1d11c8a08e900fdc8e
ehlkara <ehlullah.fb@gmail.com>
2023-11-03 23:50:44 +0300

    intro c#

--- console-programming/Program.cs
+++ console-programming/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi");
-Console.ReadLine();
+Console.WriteLine("Enter Name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter Surname:");
+string surnname = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + " " + surnname);
