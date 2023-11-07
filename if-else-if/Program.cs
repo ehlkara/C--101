727e3072ef5e23b45da95c170dd71ae86d2b4262
ehlkara <ehlullah.fb@gmail.com>
2023-11-07 15:03:34 +0300

    created if-else-if console app

---
+++ if-else-if/Program.cs
@@ -0,0 +1,13 @@
+﻿int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11)
+    Console.WriteLine("Good morning!");
+else if (time <= 18)
+    Console.WriteLine("Have a nice Day!");
+else
+    Console.WriteLine("Good night!");
+
+string result = time <= 18 ? "Have a nice Day?" : "Good Night!";
+
+result = time >= 6 && time < 11 ? "Good morning!" : time <= 18 ? "Have nice Day!" : "Good Night!";
+Console.WriteLine(result);
