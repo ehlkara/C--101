c6967ec62a968dcb77c2e5268b2f249cb626861f
ehlkara <ehlullah.fb@gmail.com>
2023-11-13 11:00:54 +0300

    created mtehods projects

---
+++ methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // access_modifier return_value method_name(parameterListes/arguement)
+            //{
+            ////commands
+            //}
+
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a + b);
+
+            int result = Sum(a, b);
+            Console.WriteLine(result);
+
+            Methods example = new Methods();
+            example.WriteConsole(Convert.ToString(result));
+
+            int result2 = example.PlusAndSum(ref a,ref b);
+            example.WriteConsole(Convert.ToString(result2));
+            example.WriteConsole(Convert.ToString(a + b));
+        }
+
+        static int Sum(int value1, int value2)
+        {
+            return (value1 + value2);
+        }
+    }
+
+    class Methods
+    {
+        public void WriteConsole(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        public int PlusAndSum(ref int value1, ref int value2)
+        {
+            value1 += 1;
+            value2 += 1;
+            return value1 + value2;
+        }
+    }
+}
